2fc75cabd47c39d70a1123e8cc7f19ee012bbada
Michael Bøje Skjærlund <michael_b_hansen@live.dk>
2020-01-05 16:21:14 +0100

    First lineup

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -3,68 +3,15 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/password-wrapper.css" />
         <title>PASSWORD HANDLER - Powered by Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
+       
+        
         <div class="flex-center position-ref full-height">
+           <!--
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -78,12 +25,42 @@
                     @endauth
                 </div>
             @endif
-
-            <div class="content">
+            -->
+            
+            <div class="container">
                 <div class="title m-b-md">
                     PASSWORD HANDLER - Powered by Laravel
                 </div>
+                <!-- Add new password -->
+                <div id="password-add-wrapper" class="outer-box row" >
+                    <div class="password-form-wrapper" >
+                        <form class="form-inline">
+                            <input type="text" name="password-assosiation-alias" class="form-control" placeholder="Username" />
+                            <input type="password" name="password" class="form-control" placeholder="Password" />
+                            <input type="submit" name="save-password" class="btn btn-primary" value="Save" />
+                        </form>
+                    </div>
+                </div>
+
+                <!-- Search password -->
+                <div id="password-search-wrapper" class="outer-box row" >
+                    <form class="form-inline">
+                        <input type="text" name="search-password" class="form-control" placeholder="Search" />
+                        <input type="submit" name="search-password-submit" class="btn btn-primary" value="Search" />
+                    </form>
+                </div>
+
+                <!-- Password list -->
+                <h2>Saved passwords list</h2>
+                <div id="password-list-wrapper" class="outer-box row" >
+                    <form class="form-inline">
+                        <input type="text" name="password-assosiation-alias" class="form-control" placeholder="Username" readonly />
+                        <input type="password" name="password" class="form-control" placeholder="Password" readonly />
+                        <input type="submit" name="save-password" class="btn btn-primary" value="Edit" />
+                    </form>
+                </div>
             </div>
+
         </div>
     </body>
 </html>

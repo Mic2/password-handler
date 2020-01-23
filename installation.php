6bbbdd8299e83a437a67b4d2fe5107ed1d9cf166
Michael Bøje Skjærlund <michael_b_hansen@live.dk>
2020-01-23 10:04:19 +0100

    Added installation script wich makes Laravel play nice after setting up container

---
+++ installation.php
@@ -0,0 +1,38 @@
+<?php 
+
+// Installation script for laravel docker image
+echo "starting installation \n";
+
+EchoCommandDescription("Starting composer update:");
+ExecuteCommand("echo y | composer update");
+
+EchoCommandDescription("Starting Laravel database migration:");
+ExecuteCommand("php artisan migrate");
+
+function EchoCommandDescription($echo) {
+    echo "\n";
+    echo $echo;
+}
+
+function ExecuteCommand($cmd) {
+  
+    echo "\n";
+
+    // Storing output of exec
+    $output = array();
+
+    // Executing the command
+    exec($cmd, $output);
+    
+    // Outputting the result of the command
+    OutputCommandResult($output);
+}
+
+function OutputCommandResult($result) {
+
+    // printing the output to terminal
+    foreach($result as $line) {
+        echo $line . "\n";
+    }
+
+}
